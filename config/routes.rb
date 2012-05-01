a1feb9823f695a022266b89002f2bffff71c4e8e
Raghuveera Betina <rbetina@gmail.com>
2012-05-01 14:45:48 -0500

    Create: route.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 TheGoldenSeven::Application.routes.draw do
   
+  post 'lines', :controller => 'lines', :action => 'create'
+  
   get 'lines/new', :controller => 'lines', :action => 'new'
   
   get 'lines', :controller => 'lines', :action => 'index'
