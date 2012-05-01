b4c29913989274c1133640d997129913815f7b04
Raghuveera Betina <rbetina@gmail.com>
2012-05-01 14:48:35 -0500

    Create: action.

--- app/controllers/lines_controller.rb
+++ app/controllers/lines_controller.rb
@@ -7,5 +7,14 @@ class LinesController < ApplicationController
   def new
     
   end
+  
+  def create
+    @line = TrainLine.new
+    @line.name = params[:name]
+    @line.frequency = params[:frequency]
+    @line.save
+    
+    redirect_to 'http://localhost:3000/lines'
+  end
     
 end
