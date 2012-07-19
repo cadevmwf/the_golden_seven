91748ccf0e4ddce0e7301b06a6faf77d0d1c3589
Raghu Betina <rbetina@gmail.com>
2012-07-19 17:59:34 -0500

    Naming routes according to conventions.

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,14 @@
 TheGoldenSeven::Application.routes.draw do
   
-  delete 'lines/:id', :controller => 'lines', :action => 'destroy'
+  delete 'lines/:id', :controller => 'lines', :action => 'destroy', :as => 'train_line'
   
   put 'lines/:id', :controller => 'lines', :action => 'update'
   
-  get 'lines/:id/edit', :controller => 'lines', :action => 'edit'
+  get 'lines/:id/edit', :controller => 'lines', :action => 'edit', :as => 'edit_train_line'
   
-  post 'lines', :controller => 'lines', :action => 'create'
+  post 'lines', :controller => 'lines', :action => 'create', :as => 'train_lines'
   
-  get 'lines/new', :controller => 'lines', :action => 'new'
+  get 'lines/new', :controller => 'lines', :action => 'new', :as => 'new_train_line'
   
   get 'lines', :controller => 'lines', :action => 'index'
   
