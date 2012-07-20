1251ee1648239f8b48dda0370e506bc312eba2cf
Raghu Betina <rbetina@gmail.com>
2012-07-20 04:37:48 -0500

    Added support for JSON responses, and bugfixes.

--- app/controllers/lines_controller.rb
+++ app/controllers/lines_controller.rb
@@ -7,7 +7,8 @@ class LinesController < ApplicationController
   end
   
   def update
-    TrainLine.find_by_id(params[:id]).update_attributes(params[:train_line])
+    @line = TrainLine.find_by_id(params[:id]).
+    @line.update_attributes(params[:train_line])
     
     redirect_to train_line_url(@line.id)
   end
@@ -18,10 +19,20 @@ class LinesController < ApplicationController
   
   def show
     @line = TrainLine.find_by_id(params[:id])
+    
+    respond_to do |format|
+      format.html
+      format.json { render :json => @line }
+    end
   end
   
   def index
     @lines = TrainLine.all
+    
+    respond_to do |format|
+      format.html
+      format.json { render :json => @lines }
+    end
   end
   
   def new
@@ -29,9 +40,9 @@ class LinesController < ApplicationController
   end
   
   def create
-    TrainLine.create(params[:train_line])
+    @line = TrainLine.create(params[:train_line])
   
-    redirect_to train_lines_url
+    redirect_to train_line_url(@line)
   end
   
 end
